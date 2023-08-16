7bf7a5fbcd0c56fa67a26aad6ac040e62f093131
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-08-16 18:16:27 +0600

    data type

---
+++ Javascript/dataType.js
@@ -0,0 +1,5 @@
+const number = 100;        // Number
+let name = "Moumita Rashid Bushra";   // String
+const isTrue = true; // Boolean
+var data = null;   // Null
+let location;               // Undefined
